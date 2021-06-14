c10f0e31dbe02fc14611dfcea49593e064f7a12f
EL ABOUDI Said <sdelaboudi@gmail.com>
2021-06-14 03:32:22 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,18 +1,19 @@
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier,_tree
+from sklearn.tree import DecisionTreeClassifier,_tree ,plot_tree,export_graphviz
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 import csv
 import re
+import nltk
+from nltk.stem import WordNetLemmatizer
 
+lemmatizer = WordNetLemmatizer()
 training = pd.read_csv('Training.csv')
 testing= pd.read_csv('Testing.csv')
 cols= training.columns
 cols= cols[:-1]
-for col in cols:
-    print(col)
 x = training[cols]
 y = training['prognosis']
 y1= y
@@ -37,7 +38,7 @@ clf = clf1.fit(x_train,y_train)
 # print ("cross result========")
 scores = cross_val_score(clf, x_test, y_test, cv=3)
 # print (scores)
-print (scores.mean())
+# print (scores.mean())
 
 
 importances = clf.feature_importances_
@@ -49,6 +50,12 @@ description_list = dict()
 precautionDictionary=dict()
 
 symptoms_dict = {}
+
+predected_diseases = []
+symptoms_present = []
+diseases = []
+precution_list = []
+
 
 def init():
     #Read from excell files
@@ -102,88 +109,93 @@ def sec_predict(symptoms_exp):
     return rf_clf.predict([input_vector])
 
 def print_disease(node):
-    print(node)
     node = node[0]
     val = node.nonzero()
     disease = le.inverse_transform(val[0])
     return disease
 
-def tree_to_code(tree, feature_names):
+def recurse(node, depth,input,feature_names,tree):
     tree_ = tree.tree_
-    feature_name = [ feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!" for i in tree_.feature ]
+    feature_name = [feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!" for i in tree_.feature]
+    if tree_.feature[node] != _tree.TREE_UNDEFINED:
+        name = feature_name[node]
+        threshold = tree_.threshold[node]
+        if name == input:
+            val = 1
+        else:
+            val = 0
+        if  val <= threshold:
+            recurse(tree_.children_left[node], depth + 1,input,feature_names,tree)
+        else:
+            symptoms_present.append(name)
+            recurse(tree_.children_right[node], depth + 1,input,feature_names,tree)
+    else:
+        present_disease = print_disease(tree_.value[node])
+        red_cols = reduced_data.columns
+        symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
+        print("Are you experiencing any ")
+        symptoms_exp=[]
+        for syms in list(symptoms_given):
+            print(syms,"? : ",end='')
+            #   if yes the append !!
+            symptoms_exp.append(syms)
 
-    chk_dis=",".join(feature_names).split(",")
-    symptoms_present = []
+        second_prediction=sec_predict(symptoms_exp)
+        if(present_disease[0]==second_prediction[0]):
+            predected_diseases.append(present_disease[0])
+        else:
+            predected_diseases.append(present_disease[0])
+            predected_diseases.append(second_prediction[0])
 
-    # conf_inp=int()
-    while True:
+def tree_to_code(tree, feature_names):
+    conf_inp=1
+    chk_dis = ",".join(feature_names).split(",")
+    while conf_inp >0:
         print("Enter the symptom you are experiencing  \n\t\t\t\t\t\t",end="->")
         disease_input = input("")
-        conf,cnf_dis=check_pattern(chk_dis,disease_input)
-        print("-----------",cnf_dis)
-        if conf==1:
-            print("searches related to input: ")
-            for num,it in enumerate(cnf_dis):
-                print(num,")",it)
-            if num!=0:
-                print(f"Select the one you meant (0 - {num}):  ", end="")
-                conf_inp = int(input(""))
-            else:
-                conf_inp=0
-            disease_input=cnf_dis[conf_inp]
-            break
-        else:
-            print("Enter valid symptom.")
+        tokens = nltk.word_tokenize(disease_input)
+        tokens = [lemmatizer.lemmatize(word) for word in tokens]
+        for w in tokens :
+            print(w)
+            conf, cnf_dis = check_pattern(chk_dis, w)  # conf,cnf_dis=check_pattern(chk_dis,disease_input)
+            if conf==1:
+                for num, it in enumerate(cnf_dis):
+                    print(num, ")", it)
 
-    def recurse(node, depth):
-        indent = "  " * depth
-        if tree_.feature[node] != _tree.TREE_UNDEFINED:
-            name = feature_name[node]
-            threshold = tree_.threshold[node]
-            if name == disease_input:
-                val = 1
-            else:
-                val = 0
-            if  val <= threshold:
-                recurse(tree_.children_left[node], depth + 1)
-            else:
-                symptoms_present.append(name)
-                recurse(tree_.children_right[node], depth + 1)
+        conf,cnf_dis=check_pattern(chk_dis,"") # conf,cnf_dis=check_pattern(chk_dis,disease_input)
+        print("searches related to input: ")
+        for num, it in enumerate(cnf_dis):
+            print(num, ")", it)
+        if num != 0:
+            print(f"Select the one you meant (0 - {num}):  ", end="")
+            try:
+                conf_inp = int(input(""))
+            except:
+                pass
         else:
-            present_disease = print_disease(tree_.value[node])
-            red_cols = reduced_data.columns
-            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero()]
-            print("Are you experiencing any ")
-            symptoms_exp=[]
-            for syms in list(symptoms_given):
-                inp=""
-                print(syms,"? : ",end='')
-                while True:
-                    inp=input("")
-                    if(inp=="yes" or inp=="no"):
-                        break
-                    else:
-                        print("provide proper answers i.e. (yes/no) : ",end="")
-                if(inp=="yes"):
-                    symptoms_exp.append(syms)
-            second_prediction=sec_predict(symptoms_exp)
-            if(present_disease[0]==second_prediction[0]):
-                print("You may have ", present_disease[0])
-                print(description_list[present_disease[0]])
-            else:
-                print("You may have ", present_disease[0], "or ", second_prediction[0])
-                print(description_list[present_disease[0]])
-                print(description_list[second_prediction[0]])
-            precution_list=precautionDictionary[present_disease[0]]
-            print("Take following measures : ")
-            for  i,j in enumerate(precution_list):
-                print(i+1,")",j)
-    recurse(0, 1)
+            conf_inp = 0
+        if conf_inp != -1 :
+            disease_input = cnf_dis[conf_inp]
+            diseases.append(disease_input)
+
+    for item in diseases:
+        recurse(0, 1,item,feature_names,tree)
+
+    for item in predected_diseases :
+        print("You may have ",item)
+        print(description_list[item])
+        precution_list.append(precautionDictionary[item])
+
+    print("Take following measures : ")
+    for j in precution_list:
+        print(j)
+
 
 init()
 tree_to_code(clf,cols)
 
-print(severityDictionary)
-print(description_list)
-print(precautionDictionary)
-print(symptoms_dict)
+
+#print(severityDictionary)
+#print(description_list)
+#print(precautionDictionary)
+#print(symptoms_dict)
